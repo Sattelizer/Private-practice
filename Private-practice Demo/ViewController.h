713906dea63c46307d2fe1460ea94b052e8d7ae7
Sattelizer <lichao3201@live.cn>
2015-06-30 15:42:10 +0800

    Create a New Project
    Create a new project with cocoapods

---
+++ Private-practice Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Private-practice Demo
+//
+//  Created by 李超 on 15/6/30.
+//  Copyright (c) 2015年 Sattelizer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Private-practice Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Private-practice Demo
+//
+//  Created by 李超 on 15/6/30.
+//  Copyright (c) 2015年 Sattelizer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
